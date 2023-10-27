49a5d66bf7cdde89603140faa267fefab788a5a6
William Thompson <wthompson@uvic.ca>
2023-10-27 16:28:17 -0700

    Update to different schema message naems

--- src/SpidersMessageEncoding.jl
+++ src/SpidersMessageEncoding.jl
@@ -5,7 +5,7 @@ evalschema(SpidersMessageEncoding, joinpath(@__DIR__, "../sbe-schemas/image.xml"
 evalschema(SpidersMessageEncoding, joinpath(@__DIR__, "../sbe-schemas/dmcommand.xml"))
 
 const MessageHeader = messageHeader
-export MessageHeader, Image, DmCommand
+export MessageHeader, ImageMessage, DmCommandMessage
 
 # Define some higher level functions to make working
 # with these types a little more convenient
@@ -32,21 +32,21 @@ function pixel_format_from_dtype(DType::Type)
     end
     error(lazy"Datatype $DType not found in list of supported pixel formats $(pixformat_pairs)")
 end
-function Base.eltype(img::Image)
+function Base.eltype(img::ImageMessage)
     return pixel_dtype_from_format(img.format)
 end
 
-function Base.size(img::Image)
+function Base.size(img::ImageMessage)
     return (img.width, img.height)
 end
 
 
 """
-Function to view an Image.frameBuffer as a 2D array of the
+Function to view an ImageMessage.frameBuffer as a 2D array of the
 correct data type and dimensions.
 
 Note: This function is not type stable since it infers the 
-data type from the `Image.format` field. See the method
+data type from the `ImageMessage.format` field. See the method
 `frameArray(DType, img)` for a type-stable version that allows
 you to assume a given format (and error if mismatched).
 
@@ -60,7 +60,7 @@ Aeron.subscribe(aeron_conf) do aeronsub
     # Get a first frame and fetch the data type 
     # dynamically
     first_frame = first(aeronsub)
-    first_img = Image(first_frame.buffer)
+    first_img = ImageMessage(first_frame.buffer)
     pixmat = framedata(first_img)
     ElType = eltype(pixmat)
     # Process the first image using this dynamically
@@ -76,7 +76,7 @@ end
 
 function letsgo(aeronsub, ElType)
     for frame in subscription
-        img = Image(first_frame.buffer)
+        img = ImageMessage(first_frame.buffer)
         # Note the extra first argument! 
         pixmat = framedata(ElType, img) 
         # use pixmat here:
@@ -90,14 +90,14 @@ function process_image(img)
 end
 ```
 """
-function framedata(img::Image)
+function framedata(img::ImageMessage)
     # Not type-stable. It's conceivable
     # that it could constant propagate in future
     # versions of Julia.
     ElType = eltype(img)
     return framedata(ElType, img)
 end
-function framedata(Eltype, img::Image)
+function framedata(Eltype, img::ImageMessage)
     reint = reinterpret(
         Eltype,
         img.frameBuffer
@@ -110,7 +110,7 @@ function framedata(Eltype, img::Image)
     return reshp
 end
 
-function framedata!(img::Image, pixmat::Matrix{ElType}) where ElType
+function framedata!(img::ImageMessage, pixmat::Matrix{ElType}) where ElType
     img.format = pixel_format_from_dtype(ElType)
     resize!(img.frameBuffer, size(pixmat,1)* size(pixmat,2)*sizeof(ElType))
     img.width = size(pixmat,1)
@@ -126,11 +126,11 @@ export framedata, framedata!
 
 """
 Convenience constructor for creating taking a 2D array and byte buffer,
-and formatting it as an `Image` with width, height, format, and data type
+and formatting it as an `ImageMessage` with width, height, format, and data type
 taken from the 2D array, then copying the data.
 """
-function Image(buffer::AbstractVector{UInt8}, pixmat::Matrix)
-    img = Image(buffer)
+function ImageMessage(buffer::AbstractVector{UInt8}, pixmat::Matrix)
+    img = ImageMessage(buffer)
     framedata!(img, pixmat)
     return img
 end
