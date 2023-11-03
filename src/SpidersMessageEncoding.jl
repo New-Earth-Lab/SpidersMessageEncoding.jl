36078677ec352ca2fea2edf143df7cf647379fe3
William Thompson <wthompson@uvic.ca>
2023-11-03 11:35:34 -0700

    Type stable access to arraydata by splitting tensor type

--- src/SpidersMessageEncoding.jl
+++ src/SpidersMessageEncoding.jl
@@ -1,13 +1,41 @@
 module SpidersMessageEncoding
 
 using SimpleBinaryEncoding
-evalschema(SpidersMessageEncoding, joinpath(@__DIR__, "../sbe-schemas/image.xml"))
-evalschema(SpidersMessageEncoding, joinpath(@__DIR__, "../sbe-schemas/dmcommand.xml"))
+# evalschema(SpidersMessageEncoding, joinpath(@__DIR__, "../sbe-schemas/image.xml"))
+# evalschema(SpidersMessageEncoding, joinpath(@__DIR__, "../sbe-schemas/dmcommand.xml"))
 evalschema(SpidersMessageEncoding, joinpath(@__DIR__, "../sbe-schemas/command.xml"))
 evalschema(SpidersMessageEncoding, joinpath(@__DIR__, "../sbe-schemas/tensor.xml"))
 
+# To set string contents without allocations, users should use StaticString.
+# Reexport the key string macros for their use.
+using StaticStrings
+export @static_str, @cstatic_str, MessageHeader, ArrayMessage, arraydata, arraydata!
+
 const MessageHeader = messageHeader
-export MessageHeader, ImageMessage, DmCommandMessage
+
+# Type alias for union of different array shapes
+
+buffertype(d::TensorMessage{T}) where T = T
+
+# We want to create copies of TensorMessage that have a specific dimensionality
+# This is to allow easier type-stable access to the underlying data for the user.
+struct ArrayMessage{N,T}
+    tensor::TensorMessage{T}
+    function ArrayMessage{N}(args...; kwargs...) where N
+        tensor = TensorMessage(args...; kwargs...)
+        return new{N,buffertype(tensor)}(tensor)
+    end
+end
+
+# Forward to tensor
+@inline Base.getproperty(msg::ArrayMessage, prop::Symbol) = @inline getproperty(getfield(msg, :tensor), prop)
+@inline Base.setproperty!(msg::ArrayMessage, prop::Symbol, value) = setproperty!(getfield(msg, :tensor), prop::Symbol, value)
+Base.sizeof(msg::ArrayMessage) = sizeof(getfield(msg, :tensor))
+Base.propertynames(msg::ArrayMessage) = propertynames(getfield(msg, :tensor))
+Base.show(io::IO, mime::MIME"text/plain", msg::ArrayMessage) = Base.show(io::IO, mime, getfield(msg, :tensor))
+
+# type alias to refer to either specified dimension ArrayMessage or TensorMessage
+ArbArrayMessage = Union{ArrayMessage, TensorMessage}
 
 # Define some higher level functions to make working
 # with these types a little more convenient
@@ -43,21 +71,20 @@ function pixel_format_from_dtype(DType::Type)
     end
     error(lazy"Datatype $DType not found in list of supported pixel formats $(pixformat_pairs)")
 end
-function Base.eltype(img::ImageMessage)
+function Base.eltype(img::ArbArrayMessage)
     return pixel_dtype_from_format(img.format)
 end
 
-function Base.size(img::ImageMessage)
-    return (img.width, img.height)
-end
+Base.ndims(::ArrayMessage{N}) where N = N
+Base.size(img::ArbArrayMessage) = Int.(img.shape[1:ndims(img)])
 
 
 """
-Function to view an ImageMessage.frameBuffer as a 2D array of the
+Function to view an TensorMessage.values as an array of the
 correct data type and dimensions.
 
 Note: This function is not type stable since it infers the 
-data type from the `ImageMessage.format` field. See the method
+data type from the `TensorMessage.format` field. See the method
 `frameArray(DType, img)` for a type-stable version that allows
 you to assume a given format (and error if mismatched).
 
@@ -71,8 +98,8 @@ Aeron.subscribe(aeron_conf) do aeronsub
     # Get a first frame and fetch the data type 
     # dynamically
     first_frame = first(aeronsub)
-    first_img = ImageMessage(first_frame.buffer)
-    pixmat = framedata(first_img)
+    first_img = TensorMessage(first_frame.buffer)
+    pixmat = arraydata(first_img)
     ElType = eltype(pixmat)
     # Process the first image using this dynamically
     # determined type. Continue after the function barrier.
@@ -87,9 +114,9 @@ end
 
 function letsgo(aeronsub, ElType)
     for frame in subscription
-        img = ImageMessage(first_frame.buffer)
+        img = TensorMessage(first_frame.buffer)
         # Note the extra first argument! 
-        pixmat = framedata(ElType, img) 
+        pixmat = arraydata(ElType, img) 
         # use pixmat here:
         process_image(pixmat)
     end
@@ -101,48 +128,59 @@ function process_image(img)
 end
 ```
 """
-function framedata(img::ImageMessage)
+function arraydata(img::ArbArrayMessage)
     # Not type-stable. It's conceivable
     # that it could constant propagate in future
     # versions of Julia.
     ElType = eltype(img)
-    return framedata(ElType, img)
+    return arraydata(ElType, img)
 end
-function framedata(Eltype, img::ImageMessage)
+function arraydata(Eltype, img::ArbArrayMessage)
     reint = reinterpret(
         Eltype,
-        img.frameBuffer
+        img.values
     )
     reshp = reshape(
         reint,
-        Int(img.width),
-        Int(img.height)
+        size(img)
     )
     return reshp
 end
 
-function framedata!(img::ImageMessage, pixmat::Matrix{ElType}) where ElType
+function arraydata!(img::ArbArrayMessage, pixdat::Array{ElType}) where ElType
     img.format = pixel_format_from_dtype(ElType)
-    resize!(img.frameBuffer, size(pixmat,1)* size(pixmat,2)*sizeof(ElType))
-    img.width = size(pixmat,1)
-    img.height = size(pixmat,2)
-    framedata(ElType, img) .= pixmat
-    return framedata(ElType, img)
+    resize!(img.values, prod(size(pixdat))*sizeof(ElType))
+    sz1 = size(pixdat,1)
+    sz2 = sz3 = sz4 = 0
+    if ndims(img) >= 2
+        sz2 = size(pixdat,2)
+    end
+    if ndims(img) >= 3
+        sz3 = size(pixdat,3)
+    end
+    if ndims(img) >= 4
+        sz4 = size(pixdat,4)
+    end
+    img.shape = (sz1,sz2,sz3,sz4)
+    arraydata(ElType, img) .= pixdat
+    return arraydata(ElType, img)
 end
 
-export framedata, framedata!
 
-# Piracy: this isn't actually type piracy since we own
-# these types, even though they were defined for us by SimpleBinaryEncoding/
+# # Piracy: this isn't actually type piracy since we own
+# # these types, even though they were defined for us by SimpleBinaryEncoding/
 
 """
 Convenience constructor for creating taking a 2D array and byte buffer,
-and formatting it as an `ImageMessage` with width, height, format, and data type
+and formatting it as an `TensorMessage` with width, height, format, and data type
 taken from the 2D array, then copying the data.
 """
-function ImageMessage(buffer::AbstractVector{UInt8}, pixmat::Matrix)
-    img = ImageMessage(buffer)
-    framedata!(img, pixmat)
+function ArrayMessage(buffer::AbstractVector{UInt8}, pixdat::Array{T,N}) where {T,N}
+    if N > 4
+        error("only up to 4D arrays are supported")
+    end
+    img = ArrayMessage{N}(buffer)
+    arraydata!(img, pixdat)
     return img
 end
 
