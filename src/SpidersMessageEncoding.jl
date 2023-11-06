a38ef9c46cbb48413e5db7090c75806ccb7677b6
William Thompson <wthompson@uvic.ca>
2023-11-06 09:00:16 -0800

    Make ArrayMessage parametric over element type

--- src/SpidersMessageEncoding.jl
+++ src/SpidersMessageEncoding.jl
@@ -9,21 +9,27 @@ evalschema(SpidersMessageEncoding, joinpath(@__DIR__, "../sbe-schemas/tensor.xml
 # To set string contents without allocations, users should use StaticString.
 # Reexport the key string macros for their use.
 using StaticStrings
-export @static_str, @cstatic_str, MessageHeader, ArrayMessage, arraydata, arraydata!
+export @static_str, @cstatic_str, MessageHeader, ArrayMessage, TensorMessage, arraydata, arraydata!
 
 const MessageHeader = messageHeader
 
-# Type alias for union of different array shapes
 
 buffertype(d::TensorMessage{T}) where T = T
+# Type alias for union of different array shapes
 
 # We want to create copies of TensorMessage that have a specific dimensionality
 # This is to allow easier type-stable access to the underlying data for the user.
-struct ArrayMessage{N,T}
-    tensor::TensorMessage{T}
-    function ArrayMessage{N}(args...; kwargs...) where N
+struct ArrayMessage{T<:Any,N,TensorType}
+    tensor::TensorMessage{TensorType}
+    function ArrayMessage{T,N}(args...; kwargs...) where {T,N}
         tensor = TensorMessage(args...; kwargs...)
-        return new{N,buffertype(tensor)}(tensor)
+        tensor.format = pixel_format_from_dtype(T) # type-unstable but should const-prop
+        return new{T,N,buffertype(tensor)}(tensor)
+    end
+    function ArrayMessage{T,N,BT}(args...; kwargs...) where {T,N,BT}
+        tensor = TensorMessage{BT}(args...; kwargs...)
+        tensor.format = pixel_format_from_dtype(T) # type-unstable but should const-prop
+        return new{T,N,BT}(tensor)
     end
 end
 
@@ -39,6 +45,30 @@ ArbArrayMessage = Union{ArrayMessage, TensorMessage}
 
 # Define some higher level functions to make working
 # with these types a little more convenient
+
+function Base.eltype(img::ArbArrayMessage)
+    return pixel_dtype_from_format(img.format)
+end
+
+# This is not type stable
+Base.ndims(ten::TensorMessage) = count(>(0), ten.shape)
+function Base.size(ten::TensorMessage)
+    if ten.shape[2] == 0
+        return (Int(ten.shape[1]),)
+    elseif ten.shape[3] == 0
+        return (Int(ten.shape[1]), Int(ten.shape[2]))
+    elseif ten.shape[4] == 0
+        return (Int(ten.shape[1]), Int(ten.shape[2]), Int(ten.shape[3]))
+    else
+        return (Int(ten.shape[1]), Int(ten.shape[2]), Int(ten.shape[3]), Int(ten.shape[4]))
+    end
+end
+# This is; hence why we created ArrayMessage. It's just a TensorMessage 
+# with the `ndims` tracked by the type-system.
+Base.ndims(::ArrayMessage{T,N}) where {T,N} = N
+Base.size(img::ArrayMessage) = Int.(img.shape[1:ndims(img)])
+
+
 
 const pixformat_pairs = (
     # From GenI cam spec:
@@ -71,37 +101,16 @@ function pixel_format_from_dtype(DType::Type)
     end
     error(lazy"Datatype $DType not found in list of supported pixel formats $(pixformat_pairs)")
 end
-function Base.eltype(img::ArbArrayMessage)
-    return pixel_dtype_from_format(img.format)
-end
 
-# This is not type stable
-Base.ndims(ten::TensorMessage) = count(>(0), ten.shape)
-function Base.size(ten::TensorMessage)
-    if ten.shape[2] == 0
-        return (Int(ten.shape[1]),)
-    elseif ten.shape[3] == 0
-        return (Int(ten.shape[1]), Int(ten.shape[2]))
-    elseif ten.shape[4] == 0
-        return (Int(ten.shape[1]), Int(ten.shape[2]), Int(ten.shape[3]))
-    else
-        return (Int(ten.shape[1]), Int(ten.shape[2]), Int(ten.shape[3]), Int(ten.shape[4]))
-    end
+function arraymessagetype(tensor::ArbArrayMessage)
+    return ArrayMessage{eltype(tensor),ndims(tensor)}
 end
-# This is; hence why we created ArrayMessage. It's just a TensorMessage 
-# with the `ndims` tracked by the type-system.
-Base.ndims(::ArrayMessage{N}) where N = N
-Base.size(img::ArrayMessage) = Int.(img.shape[1:ndims(img)])
-
 
 """
-Function to view an TensorMessage.values as an array of the
-correct data type and dimensions.
-
-Note: This function is not type stable since it infers the 
-data type from the `TensorMessage.format` field. See the method
-`frameArray(DType, img)` for a type-stable version that allows
-you to assume a given format (and error if mismatched).
+Return an `ArrayMessage` given a `TensorMessage`.
+This will encoded the element type and dimensionality
+in the type system so future usage is type-stable
+(after a function barrier)
 
 If you can assume that the data type is not changing
 while subscribed to a stream, a good pattern is to 
@@ -112,24 +121,25 @@ Aeron.subscribe(aeron_conf) do aeronsub
 
     # Get a first frame and fetch the data type 
     # dynamically
-    first_frame = first(aeronsub)
-    first_img = TensorMessage(first_frame.buffer)
-    pixmat = arraydata(first_img)
-    ElType = eltype(pixmat)
+    first_data = first(aeronsub)
+    first_message = TensorMessage(first_data.buffer)
+
+    arr_message = ArrayMessage(first_message)
+
     # Process the first image using this dynamically
     # determined type. Continue after the function barrier.
-    process_image(pixmat)
+    process_image(arr_message)
 
     # Function barrier: this lets Julia compile
     # code that is specialized to this image 
     # pixel format. After the first image,
     # this will be really fast.
-    letsgo(aeronsub, ElType)
+    letsgo(aeronsub, typeof(arr_message))
 end
 
-function letsgo(aeronsub, ElType)
-    for frame in subscription
-        img = TensorMessage(first_frame.buffer)
+function letsgo(aeronsub, ::Type{MessageType}) where MessageType
+    for frame in aeronsub
+        img = MessageType(frame.buffer)
         # Note the extra first argument! 
         pixmat = arraydata(ElType, img) 
         # use pixmat here:
@@ -138,26 +148,47 @@ function letsgo(aeronsub, ElType)
 end
 
 function process_image(img)
-    # Use image data here.
-    # typeof(img) == Matrix{Float32}
+    # Use image data here with zero-allocations
+    data = arraydata(img)
 end
 ```
 """
-function arraydata(img::ArbArrayMessage)
-    # Not type-stable. It's conceivable
-    # that it could constant propagate in future
-    # versions of Julia.
-    ElType = eltype(img)
-    return arraydata(ElType, img)
+function ArrayMessage(tensor::ArbArrayMessage)
+    return ArrayMessage{eltype(tensor),ndims(tensor)}(
+        getfield(tensor, :buffer)
+    )
 end
-function arraydata(Eltype::Type{T}, img::ArbArrayMessage) where T
+
+
+"""
+Function to view an TensorMessage.values or ArrayMessage.values
+as an array of the correct data type and dimensions.
+This function will be type-stable and allocation-free if used on
+an `ArrayMessage{ElType,Dim}` but not on a generic `TensorMessage`.
+"""
+function arraydata(img::TensorMessage)
     data = img.values
-    @boundscheck if mod(length(data), sizeof(Eltype)) != 0
+    ElType = eltype(img)
+    @boundscheck if mod(length(data), sizeof(ElType)) != 0
         error("length of data is not a multiple of the element type")
     end
-    # TODO: check element type
     reint =  @inbounds reinterpret(
-        Eltype,
+        ElType, # unstable for unspecialized TensorMessage
+        data,
+    )
+    reshp = @inbounds reshape(
+        reint,
+        size(img)
+    )
+    return reshp
+end
+function arraydata(img::ArrayMessage{T,N}) where {T,N}
+    data = img.values
+    @boundscheck if mod(length(data), sizeof(T)) != 0
+        error("length of data is not a multiple of the element type")
+    end
+    reint =  @inbounds reinterpret(
+        T, # unstable for unspecialized TensorMessage
         data,
     )
     reshp = @inbounds reshape(
@@ -167,11 +198,20 @@ function arraydata(Eltype::Type{T}, img::ArbArrayMessage) where T
     return reshp
 end
 
-function arraydata!(img::ArbArrayMessage, pixdat::Array{ElType}) where ElType
+function arraydata!(img::TensorMessage, pixdat::Array{ElType}) where ElType
+    return _arraydata!(ElType, img, pixdat)
+end
+# For ArrayMessage, the user has already specified the element type in the message type parameter
+function arraydata!(img::ArrayMessage{ElType}, pixdat::Array) where ElType
+    return _arraydata!(ElType, img, pixdat)
+end
+function _arraydata!(t::Type{ElType}, img::ArbArrayMessage, pixdat) where ElType
     img.format = pixel_format_from_dtype(ElType)
     resize!(img.values, prod(size(pixdat))*sizeof(ElType))
-    sz1 = size(pixdat,1)
-    sz2 = sz3 = sz4 = 0
+    sz1 = sz2 = sz3 = sz4 = 0
+    if ndims(img) >= 1
+        sz1 = size(pixdat,1)
+    end
     if ndims(img) >= 2
         sz2 = size(pixdat,2)
     end
@@ -182,8 +222,9 @@ function arraydata!(img::ArbArrayMessage, pixdat::Array{ElType}) where ElType
         sz4 = size(pixdat,4)
     end
     img.shape = (sz1,sz2,sz3,sz4)
-    arraydata(ElType, img) .= pixdat
-    return arraydata(ElType, img)
+    a = arraydata(img)
+    a .= pixdat
+    return a
 end
 
 
